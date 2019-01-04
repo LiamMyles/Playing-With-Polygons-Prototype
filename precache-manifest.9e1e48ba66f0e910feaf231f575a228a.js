1c5a3e9cc47498ec1ce3d53a01223ee8aea67cc1
Liam Myles <liammyles@sbcglobal.net>
2019-01-04 04:21:25 +0000

    Woops these are meant to be in here

---
+++ precache-manifest.9e1e48ba66f0e910feaf231f575a228a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e026bc0e6af5b6fa6eb",
+    "url": "/Playing-With-Polygons-Prototype/static/js/runtime~main.2e026bc0.js"
+  },
+  {
+    "revision": "61e19f91ac14fa7bdba8",
+    "url": "/Playing-With-Polygons-Prototype/static/js/main.61e19f91.chunk.js"
+  },
+  {
+    "revision": "d257a20f02bc91963a6a",
+    "url": "/Playing-With-Polygons-Prototype/static/js/1.d257a20f.chunk.js"
+  },
+  {
+    "revision": "61e19f91ac14fa7bdba8",
+    "url": "/Playing-With-Polygons-Prototype/static/css/main.c67e8adb.chunk.css"
+  },
+  {
+    "revision": "e761a9bbb32fa31849c3878637553aed",
+    "url": "/Playing-With-Polygons-Prototype/index.html"
+  }
+];
